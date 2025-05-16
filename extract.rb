7277b77bbaf273df1f4dea161a37cc686521f911
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-05-16 16:27:52 +0300

    init

--- extract.rb
+++ extract.rb
@@ -2,7 +2,7 @@ pp 'Extract'
 root_path = '/project/'
 # dir_list = Dir.glob("/project/**/")
 # (dir_list.filter { |f| !(f=~/tmp|vendor|storage|node_modules/)}).each { |x| p x }
-tests = Dir.glob("#{root_path}test/*.rb")
+tests = Dir.glob("#{root_path}test/**/*")
 root = Dir.glob("#{root_path}*")
 
 tests.each { |x| p x }
