5c09f3c5a2953746f5d3c4f666cf150115a6cffd
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-05-16 15:47:46 +0300

    init

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,7 +4,7 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
 
     dir_list = Dir.glob("#{Rails.root}/**/")
-    (dir_list.filter { |f| !(f=~/tmp|vendor|storage|node_modules/)}). each { |x| p x}
+    (dir_list.filter { |f| !(f=~/tmp|vendor|storage|node_modules/)}).each { |x| p x }
 
     get welcome_index_url
     assert_response :success
