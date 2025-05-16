c9bae211846fccc279346c2d935e38433a955cae
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-05-16 16:24:44 +0300

    init

--- extract.rb
+++ extract.rb
@@ -1,3 +1,9 @@
 pp 'Extract'
-dir_list = Dir.glob("/project/**/")
-(dir_list.filter { |f| !(f=~/tmp|vendor|storage|node_modules/)}).each { |x| p x }
+root_path = '/project/'
+# dir_list = Dir.glob("/project/**/")
+# (dir_list.filter { |f| !(f=~/tmp|vendor|storage|node_modules/)}).each { |x| p x }
+tests = Dir.glob("#{root_path}test/*.rb")
+root = Dir.glob("#{root_path}*")
+
+tests.each { |x| p x }
+root.each { |x| p x }
