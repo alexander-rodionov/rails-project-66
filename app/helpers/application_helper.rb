0973394f8c0c8afe6328af3590ccfe0cf2cd31fa
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-05-23 17:42:37 +0300

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
   def human_date(date, format = :human)
     date ? I18n.l(date, format:) : t('time.no_value')
   end
+
+  def safe_url(url)
+    uri = URI.parse(url)
+
+    uri.to_s if uri.is_a?(URI::HTTP)
+  rescue URI::InvalidURIError
+    nil
+  end
 end
