f4661413892e5495af3b82e3e000d2030b865b04
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-05-23 19:01:34 +0300

    amvera final

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "info@#{ENV.fetch('BASE_URL', 'example.com')}"
+  default from: "info@#{ENV.fetch('BASE_URL', 'http://example.com')[/http.*:\/\/(.*)/,1]}"
   layout 'mailer'
 end
